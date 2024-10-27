0ae58b0930533e7cfb73f39c8601c4302121ad66
深黑森林 <131975043+Deep-Dark-Forest@users.noreply.github.com>
2024-10-27 19:13:53 +0800

    Create fetch_closed_issues_labels.js

---
+++ fetch_closed_issues_labels.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = './';
+const repo = 'Hex-Dragon/PCL2';
+const url = `https://api.github.com/repos/${repo}/labels`;
+const excludePatterns = /➦ (删除|解锁|锁定)|投票中|新提交|社区处理中|等待提交者|等待确认|受阻碍|可合并|处理中|🚫 阻碍者|/;
+(async function () {
+  try {
+    const { data: labels } = await axios.get(url, {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `Bearer ${process.env.PAT}`,
+      },
+    });
+
+    for (const label of labels) {
+      if (excludePatterns.test(label.name)) continue;
+      const sanitizedLabel = label.name.replace(/[🟩🟪🟥🟨🚫<>"\\/:|?* ]/g, '_');
+      const fileName = `${sanitizedLabel}closed.md`;
+      const issuesUrl = `https://api.github.com/search/issues?q=repo:${repo}+is:issue+is:closed+label:"${encodeURIComponent(
+        label.name
+      )}"`;
+      const { data: issues } = await axios.get(issuesUrl, {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: `Bearer ${process.env.PAT}`,
+        },
+      });
+      fs.writeFileSync(path + fileName, `${issues.total_count}`);
+    }
+  } catch (error) {
+    console.error(`Error fetching labels or writing files: ${error.message}`);
+    process.exit(1);
+  }
+})();
